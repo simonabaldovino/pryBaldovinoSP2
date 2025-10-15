824d772e3475ee6b703e9fe639158efbd2dbe08a
simonabaldovino <baldovinosimona020@gmail.com>
2025-10-15 13:46:13 -0300

    dgv

--- pryBaldovinoCabaña/frmCabania.Designer.cs
+++ pryBaldovinoCabaña/frmCabania.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            groupBox1 = new GroupBox();
+            grpTipo = new GroupBox();
             txtDias = new TextBox();
             lblDias = new Label();
             cboPersonas = new ComboBox();
@@ -36,23 +36,33 @@
             cboTipo = new ComboBox();
             lblTipo = new Label();
             btnAceptar = new Button();
-            groupBox1.SuspendLayout();
+            grpAdicionales = new GroupBox();
+            chkTelevisor = new CheckBox();
+            chkHeladera = new CheckBox();
+            chkCocina = new CheckBox();
+            dgvDatos = new DataGridView();
+            colTipo = new DataGridViewTextBoxColumn();
+            colPersonas = new DataGridViewTextBoxColumn();
+            colDias = new DataGridViewTextBoxColumn();
+            grpTipo.SuspendLayout();
+            grpAdicionales.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvDatos).BeginInit();
             SuspendLayout();
             // 
-            // groupBox1
+            // grpTipo
             // 
-            groupBox1.Controls.Add(txtDias);
-            groupBox1.Controls.Add(lblDias);
-            groupBox1.Controls.Add(cboPersonas);
-            groupBox1.Controls.Add(lblPersonas);
-            groupBox1.Controls.Add(cboTipo);
-            groupBox1.Controls.Add(lblTipo);
-            groupBox1.Location = new Point(29, 28);
-            groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(510, 118);
-            groupBox1.TabIndex = 0;
-            groupBox1.TabStop = false;
-            groupBox1.Text = "Tipo de Cabaña";
+            grpTipo.Controls.Add(txtDias);
+            grpTipo.Controls.Add(lblDias);
+            grpTipo.Controls.Add(cboPersonas);
+            grpTipo.Controls.Add(lblPersonas);
+            grpTipo.Controls.Add(cboTipo);
+            grpTipo.Controls.Add(lblTipo);
+            grpTipo.Location = new Point(29, 28);
+            grpTipo.Name = "grpTipo";
+            grpTipo.Size = new Size(510, 118);
+            grpTipo.TabIndex = 0;
+            grpTipo.TabStop = false;
+            grpTipo.Text = "Tipo de Cabaña";
             // 
             // txtDias
             // 
@@ -110,7 +120,7 @@
             // 
             // btnAceptar
             // 
-            btnAceptar.Location = new Point(432, 186);
+            btnAceptar.Location = new Point(443, 410);
             btnAceptar.Name = "btnAceptar";
             btnAceptar.Size = new Size(96, 28);
             btnAceptar.TabIndex = 1;
@@ -118,23 +128,95 @@
             btnAceptar.UseVisualStyleBackColor = true;
             btnAceptar.Click += btnAceptar_Click;
             // 
+            // grpAdicionales
+            // 
+            grpAdicionales.Controls.Add(chkTelevisor);
+            grpAdicionales.Controls.Add(chkHeladera);
+            grpAdicionales.Controls.Add(chkCocina);
+            grpAdicionales.Location = new Point(32, 165);
+            grpAdicionales.Name = "grpAdicionales";
+            grpAdicionales.Size = new Size(134, 144);
+            grpAdicionales.TabIndex = 2;
+            grpAdicionales.TabStop = false;
+            grpAdicionales.Text = "Adicionales";
+            // 
+            // chkTelevisor
+            // 
+            chkTelevisor.AutoSize = true;
+            chkTelevisor.Location = new Point(23, 108);
+            chkTelevisor.Name = "chkTelevisor";
+            chkTelevisor.Size = new Size(71, 19);
+            chkTelevisor.TabIndex = 2;
+            chkTelevisor.Text = "Televisor";
+            chkTelevisor.UseVisualStyleBackColor = true;
+            // 
+            // chkHeladera
+            // 
+            chkHeladera.AutoSize = true;
+            chkHeladera.Location = new Point(23, 70);
+            chkHeladera.Name = "chkHeladera";
+            chkHeladera.Size = new Size(73, 19);
+            chkHeladera.TabIndex = 1;
+            chkHeladera.Text = "Heladera";
+            chkHeladera.UseVisualStyleBackColor = true;
+            // 
+            // chkCocina
+            // 
+            chkCocina.AutoSize = true;
+            chkCocina.Location = new Point(23, 35);
+            chkCocina.Name = "chkCocina";
+            chkCocina.Size = new Size(63, 19);
+            chkCocina.TabIndex = 0;
+            chkCocina.Text = "Cocina";
+            chkCocina.UseVisualStyleBackColor = true;
+            // 
+            // dgvDatos
+            // 
+            dgvDatos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvDatos.Columns.AddRange(new DataGridViewColumn[] { colTipo, colPersonas, colDias });
+            dgvDatos.Location = new Point(185, 165);
+            dgvDatos.Name = "dgvDatos";
+            dgvDatos.Size = new Size(354, 219);
+            dgvDatos.TabIndex = 3;
+            // 
+            // colTipo
+            // 
+            colTipo.HeaderText = "Tipo";
+            colTipo.Name = "colTipo";
+            // 
+            // colPersonas
+            // 
+            colPersonas.HeaderText = "Personas";
+            colPersonas.Name = "colPersonas";
+            // 
+            // colDias
+            // 
+            colDias.HeaderText = "Días";
+            colDias.Name = "colDias";
+            // 
             // frmCabania
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(567, 450);
+            Controls.Add(dgvDatos);
+            Controls.Add(grpAdicionales);
             Controls.Add(btnAceptar);
-            Controls.Add(groupBox1);
+            Controls.Add(grpTipo);
             Name = "frmCabania";
             Text = "frmCabania";
-            groupBox1.ResumeLayout(false);
-            groupBox1.PerformLayout();
+            Load += frmCabania_Load;
+            grpTipo.ResumeLayout(false);
+            grpTipo.PerformLayout();
+            grpAdicionales.ResumeLayout(false);
+            grpAdicionales.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvDatos).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
-        private GroupBox groupBox1;
+        private GroupBox grpTipo;
         private TextBox txtDias;
         private Label lblDias;
         private ComboBox cboPersonas;
@@ -142,5 +224,13 @@
         private ComboBox cboTipo;
         private Label lblTipo;
         private Button btnAceptar;
+        private GroupBox grpAdicionales;
+        private CheckBox chkTelevisor;
+        private CheckBox chkHeladera;
+        private CheckBox chkCocina;
+        private DataGridView dgvDatos;
+        private DataGridViewTextBoxColumn colTipo;
+        private DataGridViewTextBoxColumn colPersonas;
+        private DataGridViewTextBoxColumn colDias;
     }
 }
